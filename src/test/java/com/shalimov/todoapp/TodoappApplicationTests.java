836ba6ea47e93ce84c0b7b51f5bb7cc8e6e00895
Evgeny011 <ptua785@gmail.com>
2025-01-28 19:52:07 +0300

    First commit

---
+++ src/test/java/com/shalimov/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shalimov.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
